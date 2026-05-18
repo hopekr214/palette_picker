8bf718c05859bc619d90a01404b3a20ca28270b3
hopekr214 <hopekandel@gmail.com>
2026-05-18 08:03:57 -0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PalettePicker from './PalettePicker'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PalettePicker />
+  </React.StrictMode>
+)
